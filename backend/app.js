2aff8c848bd87727c4fe348c9e31f6368f7a3b1c
Naim Hossain <163743526+Naim-006@users.noreply.github.com>
2025-01-03 10:16:27 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,3 +32,11 @@ app.use('/files', fileRoutes);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
